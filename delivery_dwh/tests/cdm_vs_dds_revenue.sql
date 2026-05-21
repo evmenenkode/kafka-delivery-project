b703cc20050e7845fccf36860b6b81875edc6f3c
Denis Evmenenko <evmenenko.de@gmail.com>
2026-05-21 07:41:10 -0600

    Add dbt CI and Airflow orchestration

--- delivery_dwh/tests/cdm_vs_dds_revenue.sql
+++ delivery_dwh/tests/cdm_vs_dds_revenue.sql
@@ -1,10 +1,17 @@
 --Revenue consistency (CDM vs DDS)
 
-SELECT
-    (SELECT SUM(revenue) FROM {{ ref('cdm_daily_kpi') }}) AS cdm_rev,
-    (SELECT SUM(amount) FROM {{ ref('fct_events') }}) AS dds_rev
-WHERE ABS(
-    (SELECT SUM(revenue) FROM {{ ref('cdm_daily_kpi') }})
-    -
-    (SELECT SUM(amount) FROM {{ ref('fct_events') }})
-) > 0.01
+SELECT *
+FROM (
+    SELECT
+        d.date,
+        d.revenue AS cdm_rev,
+        f.rev AS dds_rev
+    FROM {{ ref('cdm_daily_kpi') }} d
+    LEFT JOIN (
+        SELECT DATE(event_ts) AS date, SUM(amount) AS rev
+        FROM {{ ref('fct_events') }}
+        GROUP BY 1
+    ) f
+    ON d.date = f.date
+)
+WHERE ABS(cdm_rev - dds_rev) > 0.01
